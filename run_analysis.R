6e93451f2fc54bc196f494b7aa9ae10b5e17531f
Mike Primm <mike@primmhome.com>
2015-04-26 10:00:55 -0500

    Add conditional download, progress print messages

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,27 @@
-library(plyr)
+require(plyr)
+require(data.table)
 
-# Fetch data and extract
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="data.zip", method="curl")
-unzip("data.zip")
+# Fetch data and extract, if needed
+if (!file.exists("./data.zip")) {
+    print("Data downloading")
+    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", destfile="./data.zip", method="curl")
+} else {
+    print("Data already downloaded")
+}
+if (!file.exists("./UCI HAR Dataset/train/X_train.txt")) {
+    print("Extracting data")
+    unzip("./data.zip")
+} else {
+    print("Data already extracted")
+}
 
 # Load training set data
+print("Loading training data")
 trainingSet <- read.table("./UCI HAR Dataset/train/X_train.txt")
 trainingSet_activity <- read.table("./UCI HAR Dataset/train/y_train.txt")
 trainingSet_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 # Load test data
+print("Loading test data")
 testSet <- read.table("./UCI HAR Dataset/test/X_test.txt")
 testSet_activity <- read.table("./UCI HAR Dataset/test/y_test.txt")
 testSet_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
@@ -28,27 +41,32 @@ colnames(trainingSet_subject) <- c("Subject")
 colnames(testSet_subject) <- c("Subject")
 
 # Add activity and subject columns to rest of test and training data
+print("Merging data for test and training into single table for each")
 trainingSet <- cbind(trainingSet, trainingSet_activity)
 trainingSet <- cbind(trainingSet, trainingSet_subject)
 testSet <- cbind(testSet, testSet_activity)
 testSet <- cbind(testSet, testSet_subject)
 
 # 1. Merges the training and the test sets to create one data set.
+print("Merging the training and the test sets to create one data set")
 combinedSet <- rbind(trainingSet, testSet)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 # Find columns with mean() and std() - keep Activity and Subject too
+print("Stripping all columns that are not mean() and std() data")
 filteredCols <- grepl("mean\\(\\)|std\\(\\)|Subject|Activity", names(combinedSet))
 combinedSet <- combinedSet[,filteredCols]
 
 # 3. Uses descriptive activity names to name the activities in the data set
+print("Map activity names in place of activity IDs")
 combinedSet$Activity <- factor(combinedSet$Activity, levels=activityMap$V1, labels=activityMap$V2)
 
 # 4. Appropriately labels the data set with descriptive variable names. 
 #(completed earlier)
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-combinedTable <- data.table(combinedSet)
-tidyData <- ddply(combinedTable, c("Activity","Subject"), numcolwise(mean))
-write.table(tidyData, file="tidyData.txt", sep=",", row.names=FALSE)
+print("Create tidy data set containing averages for each value, grouped by activity and subject")
+tidyData <- ddply(combinedSet, c("Activity","Subject"), numcolwise(mean))
+write.table(tidyData, file="./tidyData.txt", sep=",", row.names=FALSE)
+print("Tidy data saved to 'tidyData.txt'")
 
